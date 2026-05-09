55838ce88d619212eb85a6d47ce639cf69e99fe2
Guido <guidocalciano@gmail.com>
2026-05-09 19:22:42 -0300

    Make create order button work

--- react-ecommerce/src/pages/checkout/CheckoutPage.jsx
+++ react-ecommerce/src/pages/checkout/CheckoutPage.jsx
@@ -34,7 +34,7 @@ export function CheckoutPage({ cart, loadCart }) {
         <div className="checkout-grid">
           <OrderSummary cart={cart} loadCart={loadCart} deliveryOptions={deliveryOptions}/>
 
-        <PaymentSummary paymentSummary={paymentSummary}/>
+        <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart}/>
         </div>
       </div>
     </>
