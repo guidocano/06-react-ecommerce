7a4745640e0d564d053d4f6bfc0d45d8395b8364
Guido <guidocalciano@gmail.com>
2026-05-13 20:05:16 -0300

    Use beforeEach to remove duplication

--- react-ecommerce/src/pages/home/Product.test.jsx
+++ react-ecommerce/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from 'vitest';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import axios from 'axios';
@@ -7,9 +7,12 @@ import { Product } from './Product';
 vi.mock('axios');
 
 describe('Product component', () => {
-    it('displays the product details correctly', ()=> {
+    
+    let product 
+    let loadCart
 
-    const product=  {
+  beforeEach(() => {
+    product = {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -19,8 +22,14 @@ describe('Product component', () => {
     },
     priceCents: 1090,
     keywords: ["socks", "sports", "apparel"]
-  }
-        const loadCart =vi.fn();
+  };
+
+  loadCart= vi.fn()
+  })
+
+
+    it('displays the product details correctly', ()=> {
+
 
         render(<Product product={product} loadCart={loadCart}/>);
 
@@ -50,18 +59,6 @@ describe('Product component', () => {
 
 
     it('adds a product to the cart', async ()=> {
-            const product=  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  }
-        const loadCart =vi.fn();
 
         render(<Product product={product} loadCart={loadCart}/>);
 
@@ -70,7 +67,7 @@ describe('Product component', () => {
         await user.click(addToCartButton);
 
         expect(axios.post).toHaveBeenCalledWith(
-            '/api-cart-items', 
+            '/api/cart-items', 
             {
                 productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
                 quantity: 1
