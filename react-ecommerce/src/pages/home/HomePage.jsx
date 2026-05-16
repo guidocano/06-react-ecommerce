60dc2bd4ac99f112954df193ae27ca96c089abee
Guido <guidocalciano@gmail.com>
2026-05-16 09:40:55 -0300

    Updated communication with AWS

--- react-ecommerce/src/pages/home/HomePage.jsx
+++ react-ecommerce/src/pages/home/HomePage.jsx
@@ -19,7 +19,7 @@ export function HomePage( {cart, loadCart} ) {
 
   return (
     <>
-      <title>Ecommerce Project</title>
+      <title>Ecommerce Project 2026</title>
       <Header cart={cart}/>
       <div className="home-page">
         <ProductsGrid products={products} loadCart={loadCart}/>
